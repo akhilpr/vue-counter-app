e0065c8e6cb793008f542575071d2862f981c252
akhilpr <akhilpr6393@gmail.com.com>
2022-09-27 00:40:57 +0530

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,56 @@
+<script >
+import axios from "axios";
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+      axios.post('http://localhost:3000/updateCount', {
+      count: this.count,
+      created: []
+    })
+      .then(response => (this.info = response))
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/getCount')
+      .then(response => (this.count = response.data.data.count))
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ count }}</h1>
+    <button @click="increment()">Increment</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
